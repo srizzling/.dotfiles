edce1a52731dbf6160a91a704800efc801978a1d
Sriram Venkatesh <venksriram@gmail.com>
2018-06-11 14:52:39 +1200

    Ignore self

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ set -e
 
 # find the installers and run them iteratively
 git ls-tree --name-only -r HEAD | grep install.sh | while read -r installer; do
-	echo "› ${installer}..."
-	sh -c "$installer"
+
+        # ignore self
+        if $installer != "install.sh";
+                echo "› ${installer}..."
+	        sh -c "$installer"
+        fi
+
 done
