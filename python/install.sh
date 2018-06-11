3c7d5e41ea4eb7f9589729be3b184c57c9ddd533
Sriram Venkatesh <venksriram@gmail.com>
2018-06-11 15:14:39 +1200

    Add default python version

--- python/install.sh
+++ python/install.sh
@@ -5,3 +5,6 @@ set -e
 if which brew >/dev/null 2>&1; then
     brew install pyenv || brew upgrade pyenv
 fi
+
+# configure global python version here. bye bye python2
+ln -sf "$DOTFILES/python/python_version" "$PYENV_ROOT/.python_version"
--- python/path.zsh
+++ python/path.zsh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+
+
 # Configures the path to add the shim
 if command -v pyenv 1>/dev/null 2>&1; then
  eval "$(pyenv init -)"
+ export PYENV_ROOT="$HOME/.pyenv"
 fi
