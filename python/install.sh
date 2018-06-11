7d78e8d6b62fa2771fcccb97f58da4a459e4e43d
Sriram Venkatesh <venksriram@gmail.com>
2018-06-11 15:35:47 +1200

    Adding pyenv virutal environment setup

--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,9 @@ set -e
 
 if which brew >/dev/null 2>&1; then
     brew install pyenv || brew upgrade pyenv
+    # install 3.6.2 first
+    pyenv install 3.6.2
+    brew install pyenv-virtualenv
 fi
 
 # configure global python version here. bye bye python2
--- python/path.zsh
+++ python/path.zsh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-
-
 # Configures the path to add the shim
 if command -v pyenv 1>/dev/null 2>&1; then
  eval "$(pyenv init -)"
+ eval "$(pyenv virtualenv-init -)"
 fi
