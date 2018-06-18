47f07df9fff4cd5340819fe6220bc2e666e97100
Sriram Venkatesh <venksriram@gmail.com>
2018-06-18 22:44:56 +1200

    Adding some vscode configuration

--- python/install.sh
+++ python/install.sh
@@ -5,9 +5,16 @@ set -e
 if which brew >/dev/null 2>&1; then
     brew install pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv || brew upgrade pyenv-virtualenv
-    pyenv install 3.6.2
-    pyenv install 3.6.3
-    pyenv install 3.6.4
-    pyenv install 3.6.5
-    pyenv global 3.6.5
+else
+    if which pyenv >/dev/null 2>&1; then
+        pyenv update
+    else
+        curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    fi
 fi
+
+pyenv install 3.6.2 || true
+pyenv install 3.6.3 || true
+pyenv install 3.6.4 || true
+pyenv install 3.6.5 || true
+pyenv global 3.6.5 || true
---
+++ vscode/install.sh
@@ -0,0 +1,47 @@
+	#!/bin/sh
+	if test "$(which code)"; then
+		if [ "$(uname -s)" = "Darwin" ]; then
+			VSCODE_HOME="$HOME/Library/Application Support/Code"
+		else
+			VSCODE_HOME="$HOME/.config/Code"
+		fi
+
+
+		ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+		ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+		ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+
+
+		# from `code --list-extensions`
+		modules="
+DavidAnson.vscode-markdownlint
+EditorConfig.EditorConfig
+Equinusocio.vsc-material-theme
+PKief.material-icon-theme
+PeterJausovec.vscode-docker
+burtlo.inspec
+himanoa.Python-autopep8
+jaysonsantos.vscode-flake8
+mauve.terraform
+mohsen1.prettify-json
+ms-python.python
+ms-vscode.csharp
+ms-vscode.Go
+ms-vscode.PowerShell
+ms-vscode.sublime-keybindings
+rebornix.Ruby
+streetsidesoftware.code-spell-checker
+timonwong.shellcheck
+tomoki1207.vscode-gfm-preview
+wholroyd.jinja
+wwm.better-align
+carolynvs.dep
+casualjim.gotemplate
+foxundermoon.shell-format
+haaaad.ansible
+ipedrazas.kubernetes-snippets
+	"
+		for module in $modules; do
+			code --install-extension "$module" || true
+		done
+	fi
