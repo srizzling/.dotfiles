677c1dffa6a944c7678fe0e7d150b507cc8da68e
Sriram Venkatesh <venksriram@gmail.com>
2018-06-19 00:08:36 +1200

    Update new font usage

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,14 +2,14 @@
 
 if [ "$(uname -s)" = "Darwin" ]; then
 	if which brew >/dev/null 2>&1; then
-		brew cask install font-source-code-pro
+		brew cask install font-hack
 	else
 		echo "Couldn't install font because brew isn't installed"
 	fi
 else
-        if which dnf >/dev/nill 2>&1; then
-                sudo dnf install adobe-source-code-pro-fonts
-        else
-                echo "Not using fedora?"
+	if which dnf >/dev/nill 2>&1; then
+		sudo dnf install adobe-source-code-pro-fonts
+	else
+		echo "Not using fedora?"
 	fi
 fi
