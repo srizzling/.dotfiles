43cb954f32e0076ee4bbd5b8cd64d90203ad0722
Sriram Venkatesh <venksriram@gmail.com>
2018-06-18 22:50:09 +1200

    Don't run install script

--- run_install.sh
+++ run_install.sh
@@ -6,5 +6,7 @@ set -e
 # find the installers and run them iteratively
 git ls-tree --name-only -r HEAD | grep install.sh | while read -r installer; do
     echo "› ${installer}..."
-	sh -c "$installer"
+    if [ "run_install.sh" != "$installer" ]; then
+        sh -c "$installer"
+    fi
 done
