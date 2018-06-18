c16d85bec7ba6e4d5e7307173414388e7777a7b8
Sriram Venkatesh <venksriram@gmail.com>
2018-06-19 00:52:20 +1200

    Update install.sh

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,13 +2,13 @@
 
 if [ "$(uname -s)" = "Darwin" ]; then
 	if which brew >/dev/null 2>&1; then
-		brew cask install font-hack
+		brew cask install font-inconsolata
 	else
 		echo "Couldn't install font because brew isn't installed"
 	fi
 else
 	if which dnf >/dev/nill 2>&1; then
-		sudo dnf install adobe-source-code-pro-fonts
+		sudo dnf install -y levien-inconsolata-fonts
 	else
 		echo "Not using fedora?"
 	fi
