55aac0a8c0f679692953454d13ece6e6f030055b
Sriram Venkatesh <venksriram@gmail.com>
2018-06-19 00:08:52 +1200

    Add vscode settings

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,40 +7,14 @@ if test "$(which code)"; then
 		VSCODE_HOME="$HOME/.config/Code"
 	fi
 
-	# ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
-	# ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
-	# ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+	sed "s;/Users/sriram.venkatesh;$HOME;g" \
+		"$DOTFILES"/vscode/settings.json.tmpl >"$DOTFILES"/vscode/settings.json
 
+	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+
+	modules="$DOTFILES/vscode/modules.txt"
 	# from `code --list-extensions`
-	modules="
-DavidAnson.vscode-markdownlint
-EditorConfig.EditorConfig
-Equinusocio.vsc-material-theme
-PKief.material-icon-theme
-PeterJausovec.vscode-docker
-burtlo.inspec
-himanoa.Python-autopep8
-jaysonsantos.vscode-flake8
-mauve.terraform
-mohsen1.prettify-json
-ms-python.python
-ms-vscode.csharp
-ms-vscode.Go
-ms-vscode.PowerShell
-ms-vscode.sublime-keybindings
-rebornix.Ruby
-streetsidesoftware.code-spell-checker
-timonwong.shellcheck
-tomoki1207.vscode-gfm-preview
-wholroyd.jinja
-wwm.better-align
-carolynvs.dep
-casualjim.gotemplate
-foxundermoon.shell-format
-haaaad.ansible
-ipedrazas.kubernetes-snippets
-	"
-	for module in $modules; do
+	while read -r module; do
 		code --install-extension "$module" || true
-	done
+	done <"$modules"
 fi
