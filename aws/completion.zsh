8dc05c408bff05bf676eb3ea06b8cd506adbc1d3
Sriram Venkatesh <sriram.venkatesh@versent.com.au>
2018-07-17 11:35:02 +1000

    Configuring aws completion to work properly

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,7 +1,15 @@
 #!/bin/sh
 # shellcheck disable=SC1090
 
-# since, I'm using pyenv.. it should be loaded into the shims.. (which in theory
-# should be loaded into the path)
 
-test -e aws_zsh_completer.sh && . "$_"
+if command -v pyenv 1>/dev/null 2>&1; then
+    # Sourceing shims is bugged in pyenv so to source from shims,
+    # need to copy the completer to somewhere (using whatever is used in the version script?)
+    # and then use that instead
+
+    mkdir -p "$HOME/.zsh_completors"
+    python_version=$(pyenv global)
+    aws_completer_path="$PYENV_ROOT/versions/$python_version/bin/aws_zsh_completer.sh"
+    cp "$aws_completer_path" "$HOME/.zsh_completors/aws_zsh_completer.sh"
+    source "$HOME/.zsh_completors/aws_zsh_completer.sh"
+fi
